c4cbd600004b01ccb71c18b390290f020126ddc2
Max Chen <immaxchen@gmail.com>
2019-01-10 22:48:04 +0800

    Add files via upload

---
+++ RemoteProcessExplorer.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, send_from_directory
+import pythoncom, win32ts, wmi, json
+
+app = Flask(__name__)
+
+@app.route('/RemoteProcessExplorer')
+def index():
+    return send_from_directory('.', 'RemoteProcessExplorer.html')
+
+@app.route('/RemoteProcessExplorer/user')
+def list_user():
+    pythoncom.CoInitialize()
+    data = {}
+    for session in win32ts.WTSEnumerateSessions(win32ts.WTS_CURRENT_SERVER_HANDLE):
+        sid = session['SessionId']
+        username = win32ts.WTSQuerySessionInformation(None, sid, win32ts.WTSUserName)
+        data[sid] = username
+    return json.dump(data)
+
+@app.route('/RemoteProcessExplorer/list')
+def list_process():
+    pythoncom.CoInitialize()
+    data = []
+    for process in wmi.WMI().Win32_Process():
+        data.append([process.Name, process.SessionId, process.ProcessId])
+    return json.dump(data)
+
+@app.route('/RemoteProcessExplorer/kill/<pid>')
+def kill_process(pid):
+    pythoncom.CoInitialize()
+    process = wmi.WMI().Win32_Process(ProcessId=int(pid))[0]
+    process.Terminate()
+    return "0"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
